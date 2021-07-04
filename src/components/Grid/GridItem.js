3e47cc044561460b65504ae3264729f83252f16d
mariana <marianaferreira_17@hotmail.com>
2021-07-04 20:23:40 +0100

    localizaçao

--- src/components/Grid/GridItem.js
+++ src/components/Grid/GridItem.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"
 // nodejs library to set properties for components
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
 
 const styles = {
   grid: {
@@ -14,93 +14,93 @@ const styles = {
     paddingLeft: "15px",
     flexBasis: "auto"
   }
-};
+}
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(styles)
 
 export default function GridItem(props) {
-  const classes = useStyles();
-  const { children, className, hasTxtType, ...rest } = props;
+  const classes = useStyles()
+  const { children, className, hasTxtType, ...rest } = props
 
   const TypeWriter = function (txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = "";
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-  };
+    this.txtElement = txtElement
+    this.words = words
+    this.txt = ""
+    this.wordIndex = 0
+    this.wait = parseInt(wait, 10)
+    this.type()
+    this.isDeleting = false
+  }
 
   // Type Method
   TypeWriter.prototype.type = function () {
     // Current index of word
-    const current = this.wordIndex % this.words.length;
+    const current = this.wordIndex % this.words.length
     // Get full text of current word
-    const fullTxt = this.words[current];
+    const fullTxt = this.words[current]
 
     // Check if deleting
     if (this.isDeleting) {
       // Remove char
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+      this.txt = fullTxt.substring(0, this.txt.length - 1)
     } else {
       // Add char
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+      this.txt = fullTxt.substring(0, this.txt.length + 1)
     }
 
     // Insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
 
     // Initial Type Speed
-    let typeSpeed = 300;
+    let typeSpeed = 300
 
     if (this.isDeleting) {
-      typeSpeed /= 2;
+      typeSpeed /= 2
     }
 
     // If word is complete
     if (!this.isDeleting && this.txt === fullTxt) {
       // Make pause at end
-      typeSpeed = this.wait;
+      typeSpeed = this.wait
       // Set delete to true
-      this.isDeleting = true;
+      this.isDeleting = true
     } else if (this.isDeleting && this.txt === "") {
-      this.isDeleting = false;
+      this.isDeleting = false
       // Move to next word
-      this.wordIndex++;
+      this.wordIndex++
       // Pause before start typing
-      typeSpeed = 500;
+      typeSpeed = 500
     }
 
-    setTimeout(() => this.type(), typeSpeed);
-  };
+    setTimeout(() => this.type(), typeSpeed)
+  }
 
   const init = () => {
-    const txtElement = document.querySelector(`.txt-type-${hasTxtType}`);
-    const words = JSON.parse(txtElement.getAttribute("data-words"));
-    const wait = txtElement.getAttribute("data-wait");
+    const txtElement = document.querySelector(`.txt-type-${hasTxtType}`)
+    const words = JSON.parse(txtElement.getAttribute("data-words"))
+    const wait = txtElement.getAttribute("data-wait")
     // Init TypeWriter
-    new TypeWriter(txtElement, words, wait);
-  };
+    new TypeWriter(txtElement, words, wait)
+  }
 
   useEffect(() => {
     if (hasTxtType) {
-      init();
+      //   init();
     }
-  }, []);
+  }, [])
 
   return (
     <Grid item {...rest} className={classes.grid + " " + className}>
       {children}
     </Grid>
-  );
+  )
 }
 
 GridItem.defaultProps = {
   className: ""
-};
+}
 
 GridItem.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string
-};
+}
